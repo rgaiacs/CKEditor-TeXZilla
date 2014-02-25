6b0a14d37fda45e64f39a123fb2ac9cd06f5e6a9
Raniere Silva <raniere@ime.unicamp.br>
2014-02-25 11:52:13 -0300

    Improve in dialog window
    Replace `div` with `p` in the preview.

--- src/dialogs/interface.js
+++ src/dialogs/interface.js
@@ -99,7 +99,7 @@ CKEDITOR.dialog.add("texzillaDialog", function(editor) {
                 type: "html",
                 label: "Preview",
                 // Width of the preview box is based on the minWidth of the dialog window.
-                html: "<div><p><strong>Preview</strong>:</p><div id=\"Preview\" style=\"width:250px; overflow:auto;\"></div>",
+                html: "<div><p><strong>Preview</strong>:</p><p id=\"Preview\" style=\"width:250px; overflow:auto;\"></p></div>",
                 setup: function(insertMode, element) {
                     var preview = document.getElementById("Preview");
                     // Clean previous preview
